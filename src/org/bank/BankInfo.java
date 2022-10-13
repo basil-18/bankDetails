b1f0ee2bd1fe0a1403074a06603b16a26ea2fc4a
basil-18 <basils606@gmail.com>
2022-10-13 18:48:18 +0530

    java pro

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+public class BankInfo  {
+	public void saving() {
+		System.out.println("Saving :2,000 ");
+
+	}
+	public void fixed() {
+		System.out.println("Fixed  :10,000");
+
+	}
+	public void deposit() {
+		System.out.println("Deposit :25,000");
+
+	}
+
+}
